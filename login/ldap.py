c4a19470bd27c89dbf30f39716f20125187b51d1
LXYD001 <37770037+LXYD001@users.noreply.github.com>
2018-11-20 12:04:05 +0800

    Update ldap.py

--- login/ldap.py
+++ login/ldap.py
@@ -7,10 +7,10 @@
 # @Software: PyCharm
 from ldap3 import Connection, SUBTREE, ServerPool
 
-LDAP_SERVER_POOL = ["ldap.baijiahulian.com"]
+LDAP_SERVER_POOL = [""]
 LDAP_SERVER_PORT = 389
 ADMIN_DN = "cn=s-ldap,ou=Service Account,dc=baijiahulian,dc=com"
-ADMIN_PASSWORD = "6j8f^FjkiCsA~q7e"
+ADMIN_PASSWORD = ""
 SEARCH_BASE = "OU=newcompany,DC=baijiahulian,DC=com"
 
 
