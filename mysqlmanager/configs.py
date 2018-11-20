48bdac7838b8e764e3af809474ed0a58f84411e4
LXYD001 <37770037+LXYD001@users.noreply.github.com>
2018-11-20 12:02:45 +0800

    Update configs.py

--- mysqlmanager/configs.py
+++ mysqlmanager/configs.py
@@ -7,5 +7,3 @@
 # @Software: PyCharm
 """测试库"""
 configs = {'host': "172.21.139.9", 'user': "root", 'password': "123456", 'db': "bdg_agent", 'port': 3306, 'charset': "utf8"}
-"""正式库"""
-# configs = {'host': "rds518y58f6qah0994h3.mysql.rds.aliyuncs.com", 'user': "bdg_rw", 'password': "dd27u3p$f@9@btzH", 'db': "bdg_agent", 'port': 3306, 'charset': "utf8"}
